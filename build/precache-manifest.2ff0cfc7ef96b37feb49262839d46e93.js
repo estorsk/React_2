a1f13c2c6064b6c4e6be2b5cbb88d630e9f791b1
estorsk <61714415+estorsk@users.noreply.github.com>
2020-03-03 10:31:16 +0400

    Add files via upload

---
+++ build/precache-manifest.2ff0cfc7ef96b37feb49262839d46e93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66e4b32378752fcad5beb92d7b31b19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bde6cc625b12866379d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b7ba93fe94dbfd974061",
+    "url": "/static/js/2.359fc839.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.359fc839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bde6cc625b12866379d",
+    "url": "/static/js/main.09ea8a92.chunk.js"
+  },
+  {
+    "revision": "e0d7fe0d40f52d349ba7",
+    "url": "/static/js/runtime-main.bcf2140e.js"
+  }
+]);
